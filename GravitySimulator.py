20851bdf029e7541277f83d932b2d0f098e5f738
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2024-10-29 20:58:12 -0700

    can now add mass

--- GravitySimulator.py
+++ GravitySimulator.py
@@ -1,8 +1,15 @@
 from tkinter import *
 
 
+def isfloat(num) -> bool:
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
 class Mass:
-    def __init__(self):
+    def __init__(self, mass:int) -> None:
         pass
 
 class Simulator:
@@ -18,25 +25,33 @@ class Simulator:
         self.canvas = Canvas(self.frame)
         self.canvas.pack(side=RIGHT, fill=BOTH, expand=True)
 
+        self.masses = []
+
         self.addMassButton = Button(self.canvas, text="+", command=self.addMass)
         self.addMassButton.pack()
 
         self.root.mainloop()
     
     def addMass(self) -> None:
-        popup = Toplevel(self.canvas)
-        popup.title("Select Mass Dimensions")
-        popup.geometry("300x400")
+        self.popup = Toplevel(self.canvas)
+        self.popup.title("Select Mass Dimensions")
+        self.popup.geometry("200x100")
 
-        massText = Entry(popup)
-        massText.pack()
+        done = Button(self.popup, text="Done", command=self.closeAddMass)
 
-        mass = massText.get()
+        self.massText = Entry(self.popup)
+        self.massText.bind("<Enter>", self.closeAddMass)
 
-        massText.bind("<")
+        self.massText.pack() #TODO: change the packs into grids
+        done.pack()
 
+    def closeAddMass(self, optionalVariableWhichForSomeReasonIDKIsPassed=None) -> None: # I don't know what the second positional argument does
+                                                                                        # Other than calm down exception warnings
+        if isfloat(x := self.massText.get()):
+            self.mass = float(x)
+            self.popup.destroy()
+            self.masses.append(Mass(self.mass))
 
-#i was testing
 
 
 if __name__ == "__main__": 
