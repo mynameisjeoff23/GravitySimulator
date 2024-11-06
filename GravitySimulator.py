68abad138b928d0c1a3d3aeaa92e9bd75a16c600
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2024-11-05 18:09:00 -0800

    added gravity, began velocity

--- GravitySimulator.py
+++ GravitySimulator.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from time import time
 from random import randint
+import math
 
+G = 6.674215e-11
 
 def isfloat(num) -> bool:
     try:
@@ -59,7 +61,7 @@ class Simulator:
 
 
 class Mass:
-    def __init__(self, mass:int, main:Simulator, size=50) -> None:
+    def __init__(self, mass:int, main:Simulator, size:int=50, vI:list=[0,0]) -> None:
         x = int(main.canvas.winfo_screenwidth()/2)
         y = int(main.canvas.winfo_screenheight()/2)
         self.main = main
@@ -69,16 +71,31 @@ class Mass:
         self.x = x + randint(50 - x, x - 50)
         self.y = y + randint(50 - y, y - 50)
         self.lastTime = time()
+        self.vI = vI
+        self.AG = [0.0, 0.0]    # Acceleration due to gravity in (AGx, AGy) format
 
-    def calculateAG(self) -> None:
+    def updateAG(self) -> None:
         notPast = True
         for x in self.main.masses:
             print(f"self is x: {self is x}")
             if notPast and self is x: #made confusing for short circuit
                 notPast = False
                 continue
-            else: 
-                pass # Thats math I haven't worked out yet
+            else:   #calculate acceleration to another body
+                deltaX = x.x - self.x
+                deltaY = x.y - self.y
+                theta = math.atan2(deltaY, deltaX) # still dont know if this works but i guess well find out
+                r = math.sqrt(deltaX*deltaX + deltaY*deltaY)
+                A = G * self.mass * x.mass / (r*r)
+                self.AG[0] += A * math.cos(theta)
+                self.AG[1] += A * math.sin(theta)
+
+    def updateV(self):
+        currentTime = time()
+        deltaT = currentTime - self.lastTime
+        # vf = vi + at
+
+        pass
             
 
 
