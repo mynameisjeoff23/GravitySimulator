6cf082842473c849dea1fc68779357f018d2ebb7
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2024-11-19 15:22:21 -0800

    auto mass size scale

--- GravitySimulator.py
+++ GravitySimulator.py
@@ -52,34 +52,38 @@ class Simulator:
         if isfloat(x := self.massText.get()):
             self.mass = float(x)
             self.popup.destroy()
-            self.masses.append(Mass(self.mass, self))
+            self.masses.append(Mass(self))
             self.mass = 0
 
             for i in self.masses:
-                i.calculateAG()
+                i.updateAG()
 
 
 
 class Mass:
-    def __init__(self, mass:int, main:Simulator, size:int=50, vI:list=[0,0]) -> None:
+    def __init__(self, main:Simulator, vi:list=[0,0]) -> None:
         x = int(main.canvas.winfo_screenwidth()/2)
         y = int(main.canvas.winfo_screenheight()/2)
         self.main = main
-        self.mass = mass
-        self.size = size
+        self.mass = main.mass
+        self.size = 125 - 100/( 1 + 0.0001 * self.mass) # spooky magic numbers
         print(f"x: {x} y: {y}")
         self.x = x + randint(50 - x, x - 50)
         self.y = y + randint(50 - y, y - 50)
-        self.lastTime = time()
-        self.vI = vI
+        # self.lastTime = time()
+        self.vi = vi
         self.AG = [0.0, 0.0]    # Acceleration due to gravity in (AGx, AGy) format
+        self.updateAG(startup=True)
 
-    def updateAG(self) -> None:
+        # graphical stuff
+        self.visual = self.main.canvas.create_oval(self.x - self.size, self.y - self.size, self.x + self.size, self.y + self.size)
+
+    def updateAG(self, startup:bool=False) -> None:
         notPast = True
         for x in self.main.masses:
             print(f"self is x: {self is x}")
             if notPast and self is x: #made confusing for short circuit
-                notPast = False
+                notPast = False #skips calculating gravity when self is x
                 continue
             else:   #calculate acceleration to another body
                 deltaX = x.x - self.x
@@ -90,12 +94,14 @@ class Mass:
                 self.AG[0] += A * math.cos(theta)
                 self.AG[1] += A * math.sin(theta)
 
-    def updateV(self):
-        currentTime = time()
-        deltaT = currentTime - self.lastTime
-        # vf = vi + at
-
-        pass
+        if startup: return
+        else:   # calculate velocity and new position
+                # can't really do anything here until a  
+                # play button and handler are added
+            pass
+            # currentTime = time()
+            # deltaT = currentTime - self.lastTime
+            # vf = vi + at
             
 
 
