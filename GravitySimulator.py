bdc8b159c168816ed92c5d86c479ad2c5919f82f
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2024-12-27 00:23:42 -0800

    collisions boilerplate ready for physics

--- GravitySimulator.py
+++ GravitySimulator.py
@@ -8,7 +8,7 @@ try: ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 except: print("there was an exception trying to set dpi awareness")
 
-GRAVTIATIONAL_CONSTANT = 6.674215e-11
+G_CONST = 6.674215e-11
 timeMultiplier = 1
 
 def isfloat(num) -> bool:
@@ -96,7 +96,8 @@ class Simulator:
             #self.deltaT = (self.currentTime - self.lastTime) * timeMultiplier
             self.deltaT = 0.01694915254237288 * timeMultiplier #TODO: uncomment when done debugging
             for x in self.masses:
-                x.updateAG()
+                if x.updateAG():
+                    return
             for x in self.masses:
                 x.updatePos()
             #self.lastTime = self.currentTime
@@ -110,6 +111,17 @@ class Simulator:
             self.canvas.after(16, self.updateVisuals)
         #TODO: add another after(), fix after() call in updateCallBack()
 
+    def collide(self, obj1, obj2):
+        # TODO: calculate center of mass upon collision to be new center for largest obj
+        if obj1.mass < obj2.mass:
+            obj2.changeMass(obj1.mass + obj2.mass)
+            self.canvas.delete(obj1.visualId)
+            self.masses.pop(self.masses.index(obj1))
+        else: 
+            obj1.changeMass(obj1.mass + obj2.mass)
+            self.canvas.delete(obj2.visualId)
+            self.masses.pop(self.masses.index(obj2))
+            
 
 
 class Mass:
@@ -117,15 +129,17 @@ class Mass:
         x = int(main.canvas.winfo_screenwidth()/2) #TODO: move this shit into the simulator
         y = int(main.canvas.winfo_screenheight()/2)
         self.main = main
-        self.mass = main.mass
-        self.size = 125 - 100/( 1 + 0.0001 * self.mass) # magic number
+        self.mass = main.mass 
+        self.size = 125 - 100/( 1 + 0.0001 * self.mass) # magic number TODO: change when adding zooming to be logarithmic
         self.x = x + randint(50 - x, x - 50) #temporaritly random
         self.y = y + randint(50 - y, y - 50)
         print(f"x: {self.x} y: {self.y}\nsize: {self.size}\n{x}x{y}")
         self.deltaV = [0.0, 0.0]
         if vi == None:
             self.vi = [0.0, 0.0]
-        else: self.vi = vi
+        else: 
+            self.vi = vi
+        self.P = [(self.vi[0] * self.mass), (self.vi[1] * self.mass)] # momentum as (Px, Py)
         self.AG = [0.0, 0.0] # Acceleration due to gravity in (AGx, AGy) format
 
         # graphical stuff
@@ -135,7 +149,7 @@ class Mass:
         print("should have posted a circle")
 
         
-    def updateAG(self) -> None:
+    def updateAG(self) -> int:
         notPast = True
         self.AG[0] = 0.0
         self.AG[1] = 0.0
@@ -149,10 +163,11 @@ class Mass:
                 deltaY = i.y - self.y
                 r = math.sqrt(deltaX*deltaX + deltaY*deltaY)
 
-                if r < (self.size + i.size):
+                if r < max(self.size, i.size):
                     self.vi = [0.0, 0.0]    # TODO: handle collisions               
                     self.AG = [0.0, 0.0]
-                    return               
+                    self.main.collide(self, i)
+                    return 1              
 
                 theta = abs(math.atan(deltaY / deltaX))  # still dont know if this works but i guess we'll find out
                 
@@ -165,13 +180,16 @@ class Mass:
                 else: yDir = 0 
 
                 # (GMm/r^2) * 1/m, m being self.mass
-                a = GRAVTIATIONAL_CONSTANT * i.mass / (r*r)
+                a = G_CONST * i.mass / (r*r)
                 
                 self.AG[0] += a * math.cos(theta) * xDir
                 self.AG[1] += a * math.sin(theta) * yDir
+                
         if not(int(time())%10):
             print(f"mass: {self.mass}\ta: {self.AG[0]:.8f}, {self.AG[1]:.8f}\tv: {self.vi[0]:.8f}, {self.vi[1]:.8f}\tpos: ({self.x:.8f}, {self.y:.8f})")
             print(f"deltaxy: ({deltaX:.8f}, {deltaY:.8f})\ttheta: {theta*180/math.pi:.8f}°\tr: {r}\tdir: ({xDir}, {yDir})")
+        
+        return 0
     
     def updatePos(self):
 
@@ -188,6 +206,14 @@ class Mass:
         self.vi[0] += self.deltaV[0]
         self.vi[1] += self.deltaV[1]
 
+        self.P[0] = (self.vi[0] * self.mass)
+        self.P[1] = (self.vi[1] * self.mass)
+    
+    def changeMass(self, new):
+        self.mass = new
+        self.size = 125 - 100/( 1 + 0.0001 * self.mass)
+        self.main.canvas.coords(self.visualId, self.x - self.size, self.y - self.size, self.x + self.size, self.y + self.size)
+        
 
 
 if __name__ == "__main__": 
