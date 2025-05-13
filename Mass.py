6950e66b5c649b8baaea908561f68abcc68821b9
mynameisjeoff23 <88678679+mynameisjeoff23@users.noreply.github.com>
2025-05-13 09:13:27 -0700

    abstraced Mass class removed it's dependency on Simulator

---
+++ Mass.py
@@ -0,0 +1,21 @@
+from tkinter import Canvas as Canvas
+from math import log
+class Mass:
+    
+    def __init__(self, initialXY:tuple[float, float], vi:list[float], scale:float, xOffset:float, yOffset:float, \
+                 canvas:Canvas, deltaT:float, mass:float):
+        
+        self.mass = mass
+        self.size = 10 * log(self.mass + 250) - 30.21461
+
+        self.x = initialXY[0] / scale - xOffset
+        self.y = initialXY[1] / scale - yOffset
+
+        self.deltaV = [0.0, 0.0]
+        self.vi = vi
+        self.P = [0.0, 0.0] # momentum as (Px, Py)
+        self.AG = [0.0, 0.0] # Acceleration due to gravity in (AGx, AGy) format
+
+        self.visualId = canvas.create_oval(self.x - self.size, self.y - self.size, \
+                                                     self.x + self.size, self.y + self.size, \
+                                                     fill="black", outline="black")
